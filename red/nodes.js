3b5864125c21b32bc28629c3b39579b7a308e548
eleventigers <phonodelik@gmail.com>
2013-11-19 00:25:45 +0000

    cleanup

--- red/nodes.js
+++ red/nodes.js
@@ -1,42 +1,48 @@
 /**
  * Copyright 2013 IBM Corp.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-var util = require("util"),
-    EventEmitter = require("events").EventEmitter,
-    fs = require("fs"),
-    path = require("path"),
-    events = require("./events"),
+
+'use strict';
+
+var util = require('util'),
+    EventEmitter = require('events').EventEmitter,
+    fs = require('fs'),
+    path = require('path'),
+    events = require('./events'),
     Node = require('./node'),
-    storage = null;
+    storage = null,
+    credentials = {},
+    activeConfig = [],
+    missingTypes = [],
+    activeNodesRegistry,
+    nodeTypesRegistry,
+    consoleLogHandler;
+
 
 function getCallerFilename(type) {
-    //if (type == "summary") {
-    //    var err = new Error();
-    //    console.log(err.stack);
-    //    return null;
-    //}
+    var origPrepareStackTrace, err, stack;
     // Save original Error.prepareStackTrace
-    var origPrepareStackTrace = Error.prepareStackTrace;
+    origPrepareStackTrace = Error.prepareStackTrace;
     // Override with function that just returns `stack`
     Error.prepareStackTrace = function (_, stack) {
         return stack;
-    }
+    };
     // Create a new `Error`, which automatically gets `stack`
-    var err = new Error();
+    err = new Error();
     // Evaluate `err.stack`, which calls our new `Error.prepareStackTrace`
-    var stack = err.stack;
+    stack = err.stack;
     // Restore original `Error.prepareStackTrace`
     Error.prepareStackTrace = origPrepareStackTrace;
     // Remove superfluous function call on stack
@@ -45,85 +51,164 @@ function getCallerFilename(type) {
     return stack[0].getFileName();
 }
 
-var registry = (function() {
-        var nodes = {};
-        var logHandlers = [];
-        var obj = {
-            add: function(n) {
-                nodes[n.id] = n;
-                n.on("log",function(msg) {
-                    for (var i in logHandlers) {
-                        logHandlers[i].emit("log",msg);
-                    }
-                });
-            },
-            get: function(i) {
-                return nodes[i];
-            },
-            clear: function() {
-                events.emit("nodes-stopping");
-                for (var n in nodes) {
-                    nodes[n].close();
-                }
-                events.emit("nodes-stopped");
-                nodes = {};
-            },
-            each: function(cb) {
-                for (var n in nodes) {
-                    cb(nodes[n]);
-                }
-            },
-            addLogHandler: function(handler) {
-                logHandlers.push(handler);
+function parseConfig() {
+    missingTypes = [];
+    for (var i in activeConfig) {
+        var type = activeConfig[i].type;
+        // TODO: remove workspace in next release+1
+        if (type !== 'workspace' && type !== 'tab') {
+            var nt = nodeTypesRegistry.get(type);
+            if (!nt && missingTypes.indexOf(type) === -1) {
+                missingTypes.push(type);
             }
         }
-        return obj;
+    }
+
+    if (missingTypes.length > 0) {
+        util.log('[red] Waiting for missing types to be registered:');
+        for (var i in missingTypes) {
+            util.log('[red]  - '+missingTypes[i]);
+        }
+        return;
+    }
+
+    util.log('[red] Starting flows');
+    events.emit('nodes-starting');
+    for (var i in activeConfig) {
+        var nn = null;
+        // TODO: remove workspace in next release+1
+        if (activeConfig[i].type !== 'workspace' && activeConfig[i].type !== 'tab') {
+            var nt = nodeTypesRegistry.get(activeConfig[i].type);
+            if (nt) {
+                try {
+                    nn = new nt(activeConfig[i]);
+                }
+                catch (err) {
+                    util.log('[red] '+activeConfig[i].type+' : '+err);
+                }
+            }
+            // console.log(nn);
+            if (nn == null) {
+                util.log('[red] unknown type: '+activeConfig[i].type);
+            }
+        }
+    }
+    // Clean up any orphaned credentials
+    var deletedCredentials = false;
+    for (var c in credentials) {
+        var n = activeNodesRegistry.get(c);
+        if (!n) {
+            deletedCredentials = true;
+            delete credentials[c];
+        }
+    }
+    if (deletedCredentials) {
+        storage.saveCredentials(credentials);
+    }
+    events.emit('nodes-started');
+}
+
+function stopFlows() {
+    if (activeConfig && activeConfig.length > 0) {
+        util.log('[red] Stopping flows');
+    }
+    activeNodesRegistry.clear();
+}
+
+activeNodesRegistry = (function() {
+    var nodes = {},
+        logHandlers = [];
+
+    return {
+        add: function(n) {
+            nodes[n.id] = n;
+            n.on('log',function(msg) {
+                for (var i in logHandlers) {
+                    logHandlers[i].emit('log',msg);
+                }
+            });
+        },
+        get: function(i) {
+            return nodes[i];
+        },
+        clear: function() {
+            events.emit('nodes-stopping');
+            for (var n in nodes) {
+                nodes[n].close();
+            }
+            events.emit('nodes-stopped');
+            nodes = {};
+        },
+        each: function(cb) {
+            for (var n in nodes) {
+                cb(nodes[n]);
+            }
+        },
+        addLogHandler: function(handler) {
+            logHandlers.push(handler);
+        }
+    };
 })();
 
-var ConsoleLogHandler = new EventEmitter();
-ConsoleLogHandler.on("log",function(msg) {
-    util.log("["+msg.level+"] ["+msg.type+":"+(msg.name||msg.id)+"] "+msg.msg);
+nodeTypesRegistry = (function() {
+    var node_types = {},
+        node_configs = {};
+
+    return {
+        register: function(type,node) {
+            util.inherits(node, Node);
+            var callerFilename = getCallerFilename(type);
+            if (callerFilename == null) {
+                util.log('['+type+'] unable to determine filename');
+            } else {
+                var configFilename = callerFilename.replace(/\.js$/,'.html');
+                if (fs.existsSync(configFilename)) {
+                    node_types[type] = node;
+                    if (! node_configs[configFilename]) {
+                        node_configs[configFilename] = fs.readFileSync(configFilename,'utf8');
+                    }
+                    events.emit('type-registered',type);
+                } else {
+                    util.log('['+type+'] missing template file: '+configFilename);
+                }
+            }
+        },
+        get: function(type) {
+            return node_types[type];
+        },
+        getNodeConfigs: function() {
+            var result = '';
+            for (var nt in node_configs) {
+                result += node_configs[nt];
+            }
+            return result;
+        }
+    };
+})();
+
+events.on('type-registered',function(type) {
+    if (missingTypes.length > 0) {
+        var i = missingTypes.indexOf(type);
+        if (i != -1) {
+            missingTypes.splice(i,1);
+            util.log('[red] Missing type registered: '+type);
+        }
+        if (missingTypes.length == 0) {
+            parseConfig();
+        }
+    }
 });
 
-registry.addLogHandler(ConsoleLogHandler);
+consoleLogHandler = new EventEmitter();
+consoleLogHandler.on('log',function(msg) {
+    util.log('['+msg.level+'] ['+msg.type+':'+(msg.name||msg.id)+'] '+msg.msg);
+});
 
-var node_type_registry = (function() {
-        var node_types = {};
-        var node_configs = {};
-        var obj = {
-            register: function(type,node) {
-                util.inherits(node, Node);
-                var callerFilename = getCallerFilename(type);
-                if (callerFilename == null) {
-                    util.log("["+type+"] unable to determine filename");
-                } else {
-                    var configFilename = callerFilename.replace(/\.js$/,".html");
-                    if (fs.existsSync(configFilename)) {
-                        node_types[type] = node;
-                        if (! node_configs[configFilename]) {
-                            node_configs[configFilename] = fs.readFileSync(configFilename,'utf8');
-                        }
-                        events.emit("type-registered",type);
-                    } else {
-                        util.log("["+type+"] missing template file: "+configFilename);
-                    }
-                }
-            },
-            get: function(type) {
-                return node_types[type];
-            },
-            getNodeConfigs: function() {
-                var result = "";
-                for (var nt in node_configs) {
-                    result += node_configs[nt];
-                }
-                return result;
-            }
-        }
-        return obj;
-})();
+activeNodesRegistry.addLogHandler(consoleLogHandler);
 
-var credentials = {};
+module.exports.registerType = nodeTypesRegistry.register;
+module.exports.getNodeConfigs = nodeTypesRegistry.getNodeConfigs;
+module.exports.addLogHandler = activeNodesRegistry.addLogHandler;
 
 module.exports.addCredentials = function(id,creds) {
     credentials[id] = creds;
@@ -143,10 +228,6 @@ module.exports.createNode = function(node, options) {
     Node.call(node, options);
 };
 
-module.exports.registerType = node_type_registry.register;
-module.exports.getNodeConfigs = node_type_registry.getNodeConfigs;
-module.exports.addLogHandler = registry.addLogHandler;
-
 module.exports.load = function(settings) {
     function loadNodes(dir) {
         fs.readdirSync(dir).sort().filter(function(fn){
@@ -156,7 +237,7 @@ module.exports.load = function(settings) {
                         try {
                             require(path.join(dir,fn));
                         } catch(err) {
-                            util.log("["+fn+"] "+err);
+                            util.log('['+fn+'] '+err);
                             //console.log(err.stack);
                         }
                     }
@@ -164,49 +245,26 @@ module.exports.load = function(settings) {
                     // Ignore /.dirs/ and /lib/
                     if (!/^(\..*|lib|icons)$/.test(fn)) {
                         loadNodes(path.join(dir,fn));
-                    } else if (fn === "icons") {
-                        events.emit("node-icon-dir",path.join(dir,fn));
+                    } else if (fn === 'icons') {
+                        events.emit('node-icon-dir',path.join(dir,fn));
                     }
                 }
         });
     }
-    loadNodes(__dirname+"/../nodes");
+    loadNodes(__dirname+'/../nodes');
     if (settings.nodesDir) {
         loadNodes(settings.nodesDir);
     }
-    //events.emit("nodes-loaded");
-}
-
-var activeConfig = [];
-var missingTypes = [];
-
-events.on('type-registered',function(type) {
-        if (missingTypes.length > 0) {
-            var i = missingTypes.indexOf(type);
-            if (i != -1) {
-                missingTypes.splice(i,1);
-                util.log("[red] Missing type registered: "+type);
-            }
-            if (missingTypes.length == 0) {
-                parseConfig();
-            }
-        }
-});
+    //events.emit('nodes-loaded');
+};
 
 module.exports.getNode = function(nid) {
-    return registry.get(nid);
+    return activeNodesRegistry.get(nid);
 };
 
 module.exports.addNode = function (node) {
-    return registry.add(node);
+    return activeNodesRegistry.add(node);
 };
-
-function stopFlows() {
-    if (activeConfig&&activeConfig.length > 0) {
-        util.log("[red] Stopping flows");
-    }
-    registry.clear();
-}
 
 module.exports.stopFlows = stopFlows;
 
@@ -216,72 +274,18 @@ module.exports.setConfig = function(conf) {
 
     if (!storage) {
         // Do this lazily to ensure the storage provider as been initialised
-        storage = require("./storage");
+        storage = require('./storage');
     }
     storage.getCredentials().then(function(creds) {
         credentials = creds;
         parseConfig();
     }).otherwise(function(err) {
-        util.log("[red] Error loading credentials : "+err);
+        util.log('[red] Error loading credentials : '+err);
     });
-}
+};
 
 module.exports.getConfig = function() {
     return activeConfig;
-}
+};
 
-var parseConfig = function() {
-    missingTypes = [];
-    for (var i in activeConfig) {
-        var type = activeConfig[i].type;
-        // TODO: remove workspace in next release+1
-        if (type != "workspace" && type != "tab") {
-            var nt = node_type_registry.get(type);
-            if (!nt && missingTypes.indexOf(type) == -1) {
-                missingTypes.push(type);
-            }
-        }
-    };
-    if (missingTypes.length > 0) {
-        util.log("[red] Waiting for missing types to be registered:");
-        for (var i in missingTypes) {
-            util.log("[red]  - "+missingTypes[i]);
-        }
-        return;
-    }
 
-    util.log("[red] Starting flows");
-    events.emit("nodes-starting");
-    for (var i in activeConfig) {
-        var nn = null;
-        // TODO: remove workspace in next release+1
-        if (activeConfig[i].type != "workspace" && activeConfig[i].type != "tab") {
-            var nt = node_type_registry.get(activeConfig[i].type);
-            if (nt) {
-                try {
-                    nn = new nt(activeConfig[i]);
-                }
-                catch (err) {
-                    util.log("[red] "+activeConfig[i].type+" : "+err);
-                }
-            }
-            // console.log(nn);
-            if (nn == null) {
-                util.log("[red] unknown type: "+activeConfig[i].type);
-            }
-        }
-    }
-    // Clean up any orphaned credentials
-    var deletedCredentials = false;
-    for (var c in credentials) {
-        var n = registry.get(c);
-        if (!n) {
-            deletedCredentials = true;
-            delete credentials[c];
-        }
-    }
-    if (deletedCredentials) {
-        storage.saveCredentials(credentials);
-    }
-    events.emit("nodes-started");
-}
